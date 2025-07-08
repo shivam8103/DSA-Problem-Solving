3b4ea1199d79daff1af26fe692b1760d1ecfca3b
Shivam Sinha <shivam.sinha0810@gmail.com>
2025-07-09 02:00:48 +0530

    Update 9 july

---
+++ Array/002 - Second Largest Element/Optimized.java
@@ -0,0 +1,25 @@
+public class Optimized {
+    public static int secondLargest(int[] arr) {
+        int slar = -1;
+        int lar = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > lar) {
+                slar = lar;
+                lar = arr[i];
+            } 
+            else if (arr[i] < lar & arr[i] > slar) {
+                slar = arr[i];
+            }
+
+        }
+        return slar;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 8, 8, 7, 6, 5 };
+
+        System.out.println(secondLargest(arr));
+    }
+}
