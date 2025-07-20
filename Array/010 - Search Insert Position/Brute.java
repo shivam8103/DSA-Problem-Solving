5fa7c2d217da9d54c7da113427a4ed6d7a7ed911
Shivam Sinha <shivam.sinha0810@gmail.com>
2025-07-21 00:40:50 +0530

    010

--- Array/010 - Search Insert Position/Brute.java
+++ Array/010 - Search Insert Position/Brute.java
@@ -1,3 +1,20 @@
 public class Brute {
-    
+    public static int searchInsertPosition(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                return i;
+            else if (nums[i] > target)
+                return i;
+        }
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 3, 5, 6 };
+        int target = 3;
+        // int target = 4;
+        // int target = 6;
+        // int target = 7;
+        System.out.println(searchInsertPosition(arr, target));
+    }
 }
--- Array/010 - Search Insert Position/Optimial.java
+++ Array/010 - Search Insert Position/Optimial.java
@@ -1,3 +1,31 @@
 public class Optimial {
+    public static int searchInsertPosition(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+
+            int mid = (high + low) / 2;
+
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return high+1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 3, 5, 6 };
+        // int target = 3;
+        // int target = 4;
+        // int target = 6;
+        int target = 7;
+        System.out.println(searchInsertPosition(arr, target));
+    }
     
 }
