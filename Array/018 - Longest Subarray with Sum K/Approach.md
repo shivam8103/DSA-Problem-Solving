2391cac23e31af56fb19b7c20a20ea97b2f0ee2c
Shivam Sinha <shivam.sinha0810@gmail.com>
2025-08-03 02:04:53 +0530

    018 - Brute Force only

---
+++ Array/018 - Longest Subarray with Sum K/Approach.md
@@ -0,0 +1,45 @@
+# Brute Force Approach - Try every possible Subarray
+<details>
+
+1. Try every possible subarray
+
+```
+    (1,1) , (1,2), (1,3), (1,n)
+            (2,2), (2,3), (2,n)
+                   (3,3), (3,n)
+                     .....(n,n)
+```
+
+2.  Do 2 loops to create every subarray possible
+
+
+```
+for (i=0 -> arr.length){
+    for(j=i -> arr.length){
+
+    }
+}
+```
+
+3. Find sum using another loop
+``` 
+for(k=i -> j+1){
+    sum
+}
+```
+
+4. After calculating sum. Check if sum is equal to key or not. 
+5. If sum is equal to key, then also check if length of current subarray is greater than previous length or not. 
+6. Return length or a new resultant array from `(i -> j+1)`
+
+### Time Complexity = `O(n*n*n)`
+### Space Complexity = `O(1)`
+</details>
+
+## Brute Force 2
+<details>
+
+1. To reduce time complexity, we can calculate sum along with the 2nd loop and not create a 3rd loop for sum calculation.
+### Time Complexity = `O(n*n)`
+### Space Complexity = `O(1)`
+</details>
---
+++ Array/018 - Longest Subarray with Sum K/Brute.java
@@ -0,0 +1,38 @@
+public class Brute {
+    public static int[] longestSubArray(int[] arr, int key) {
+        int length = 0;
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int sum = 0;
+                for (int k = i; k < j + 1; k++) {
+                    sum = sum + arr[k];
+                }
+                if (sum == key && length < (j - i + 1)) {
+                    length = j - i + 1;
+                    start = i;
+                    end = j + 1;
+                }
+            }
+        }
+
+        int[] result = new int[length];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arr[start];
+            start++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 10, 5, 2, 7, 1};
+        int k = 15;
+        int result[] = longestSubArray(arr, k);
+
+        for (int x : result) {
+            System.out.print(x + " ");
+        }
+    }
+}
